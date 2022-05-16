b6c106b8f8f5f835d3a6eca087897d9458118ec9
serhat köse <36682356+serhat-kose@users.noreply.github.com>
2022-05-16 23:42:25 +0300

    cart-service added

---
+++ cart-service/cart-service/src/test/java/com/serhat/cartservice/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhat.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
