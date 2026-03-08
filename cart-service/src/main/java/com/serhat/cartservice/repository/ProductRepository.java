21a2e5c8fb12b8888c7e6b35dd65096d6fd6d2a4
serhat-kose <serhatkse94@gmail.com>
2026-03-09 00:13:22 +0300

    notification service changes on micro-services processing

--- cart-service/src/main/java/com/serhat/cartservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.serhat.cartservice.repository;
-
-import com.serhat.cartservice.entity.*;
-import org.springframework.data.jpa.repository.*;
-
-public interface ProductRepository  extends JpaRepository<Product,Long> {
-}
